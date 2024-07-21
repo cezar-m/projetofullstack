2f6f63af493aae93e26ce3854b6025c0582b4e72
cezar-m <103004018+cezar-m@users.noreply.github.com>
2024-07-21 20:54:57 -0300

    Add files via upload

--- backend/connection/Conexao.js
+++ backend/connection/Conexao.js
@@ -1,1 +1,18 @@
+import mysql from 'mysql';
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'autenticacao_db'
+});
+
+connection.connect(erro => {
+	if(erro) {
+		console.error('Erro de conexão', erro);
+	    return;
+	}
+	console.log('Conexão realizada com sucesso com banco de dados!!!');
+});
+
+export default connection;
